65566d649844b4d1786e90ca882a10c431115c2d
abhimangalms <abhimangalms@gmail.com>
2018-10-16 13:12:58 +0530

    Back button added by setDisplayHomeAsUpEnabled(true)

--- app/src/main/java/com/codes/abhimangalms/bottomnavigationapp/SettingsActivity.java
+++ app/src/main/java/com/codes/abhimangalms/bottomnavigationapp/SettingsActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
